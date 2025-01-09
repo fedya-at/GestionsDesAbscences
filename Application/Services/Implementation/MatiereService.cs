da5a3e1c01ff683409d79c9e21982c19c70de880
fadia alatrech <alatrech.fadya09@gmail.com>
2025-01-09 20:24:51 +0100

    migrations

--- Application/Services/Implementation/MatiereService.cs
+++ Application/Services/Implementation/MatiereService.cs
@@ -2,7 +2,7 @@
 using Application.Services.Interfaces;
 using AutoMapper;
 using Core.Entities;
-using Core.Interfaces.IRepositories;
+using Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,46 +13,67 @@ namespace Application.Services.Implementation
 {
     public class MatiereService : IMatiereService
     {
-        private readonly IMatiereRepository _matiereRepository;
-        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public MatiereService(IMatiereRepository matiereRepository, IMapper mapper)
+        public MatiereService(IUnitOfWork unitOfWork)
         {
-            _matiereRepository = matiereRepository;
-            _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
-
-        public async Task<IEnumerable<MatiereDTO>> GetAllMatieresAsync()
-        {
-            var matieres = await _matiereRepository.ListAllAsync();
-            return _mapper.Map<IEnumerable<MatiereDTO>>(matieres);
-        }
-
-        public async Task<MatiereDTO> GetMatiereByIdAsync(int id)
-        {
-            var matiere = await _matiereRepository.GetByIdAsync(id);
-            return _mapper.Map<MatiereDTO>(matiere);
-        }
-
         public async Task AddMatiereAsync(MatiereDTO matiereDto)
         {
-            var matiere = _mapper.Map<Matiere>(matiereDto);
-            await _matiereRepository.AddAsync(matiere);
-        }
-
-        public async Task UpdateMatiereAsync(MatiereDTO matiereDto)
-        {
-            var matiere = _mapper.Map<Matiere>(matiereDto);
-            await _matiereRepository.UpdateAsync(matiere);
+            var matiere = new Matiere
+            {
+                CodeMatiere = matiereDto.CodeMatiere,
+                NomMatiere = matiereDto.NomMatiere,
+                NbreHeureCoursParSemaine = matiereDto.NbreHeureCoursParSemaine,
+                NbreHeureTDParSemaine = matiereDto.NbreHeureTDParSemaine,
+                NbreHeureTPParSemaine = matiereDto.NbreHeureTPParSemaine
+            };
+            await _unitOfWork.Matieres.AddAsync(matiere);
+            await _unitOfWork.SaveChangesAsync();
         }
 
         public async Task DeleteMatiereAsync(int id)
         {
-            var matiere = await _matiereRepository.GetByIdAsync(id);
-            if (matiere != null)
+            var matiere = await _unitOfWork.Matieres.GetByIdAsync(id);
+            if (matiere == null) throw new KeyNotFoundException("Matiere not found");
+            _unitOfWork.Matieres.Delete(matiere);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<MatiereDTO>> GetAllMatieresAsync()
+        {
+            var matieres = await _unitOfWork.Matieres.GetAllAsync();
+            return matieres.Select(m => new MatiereDTO
             {
-                await _matiereRepository.DeleteAsync(matiere);
-            }
+                CodeMatiere = m.CodeMatiere,
+                NomMatiere = m.NomMatiere,
+                NbreHeureCoursParSemaine = m.NbreHeureCoursParSemaine,
+                NbreHeureTDParSemaine = m.NbreHeureTDParSemaine,
+                NbreHeureTPParSemaine = m.NbreHeureTPParSemaine
+            });
+        }
+
+        public async Task<MatiereDTO> GetMatiereByIdAsync(int id)
+        {
+            var matiere = await _unitOfWork.Matieres.GetByIdAsync(id);
+            if (matiere == null) throw new KeyNotFoundException("Matiere not found");
+            return new MatiereDTO
+            {
+                CodeMatiere = matiere.CodeMatiere,
+                NomMatiere = matiere.NomMatiere,
+                NbreHeureCoursParSemaine = matiere.NbreHeureCoursParSemaine,
+                NbreHeureTDParSemaine = matiere.NbreHeureTDParSemaine,
+                NbreHeureTPParSemaine = matiere.NbreHeureTPParSemaine
+            };
+        }
+
+        public async Task UpdateMatiereAsync(MatiereDTO matiereDto)
+        {
+            var matiere = await _unitOfWork.Matieres.GetByIdAsync(matiereDto.CodeMatiere);
+            if (matiere == null) throw new KeyNotFoundException("Matiere not found");
+            matiere.NomMatiere = matiereDto.NomMatiere;
+            matiere.NbreHeureCoursParSemaine = matiereDto.NbreHeureCoursParSemaine;
         }
     }
 }
